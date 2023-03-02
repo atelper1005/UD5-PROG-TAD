264f3dab44f7f91ed0c2a07f02d53e8715bd0da4
atelper1005 <atelper1005@g.educaand.es>
2023-03-02 13:59:57 +0100

    Ejercicio5_2, segundo intento

--- Ejercicio5_2.java
+++ Ejercicio5_2.java
@@ -1,20 +1,33 @@
 package ejercicios;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Ejercicio5_2 {
     public static void main(String[] args) {
-        List<Alumno> listaIniciacion = new ArrayList<>();
-        listaIniciacion.add(new Alumno("pibe1", 0f, 0));
-        listaIniciacion.add(new Alumno("piba1", 0f, 0));
-
-        String nuevaTarea = "Tarea 1";
-        agregarTarea(tareas, nuevaTarea);
-        System.out.println(tareas);
-
-        String tareaBorrar = "Tarea 1";
-        agregarTarea(tareas, nuevaTarea);
-        System.out.println(tareas);
+        System.out.println("           Lista de Tareas   ");
+        TaskList tareas = new TaskList("ArrayList");
+        System.out.println("Opciones disponibles");
+        System.out.println("__________________________________");
+        System.out.println("1. Agrega una nueva tarea a la lista");
+        System.out.println();
+        TaskList.addTask(listaTareas"Tomar apuntes");
+        TaskList.addTask(lista, "Apuntar posibles dudas");
+        TaskList.addTask(lista, "Realizar los ejercicios correspondientes");
+        TaskList.addTask(lista, "Preguntar las dudas al profesor");
+        TaskList.addTask(lista, "Estudiar para el examen");
+        System.out.println("2. Eliminar una tarea de la lista");
+        System.out.println();
+        TaskList.removeTask(lista, "Preguntar las dudas al profesor");
+        TaskList.removeTask(lista, "Apuntar posibles dudas");
+        System.out.println();
+        System.out.println("3. Completar alguna de las tareas");
+        System.out.println();
+        TaskList.completeTask(lista, "Tomar apuntes");
+        System.out.println();
+        System.out.println("4. Imprimir todas las tareas");
+        System.out.println();
+        for (String e : lista) {
+            System.out.println(e);
+        }
     }
 }
--- TaskList.java
+++ TaskList.java
@@ -1,4 +1,37 @@
 package ejercicios;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class TaskList {
+
+    public TaskList(String tipoLista) {
+        List<String> listaTareas;
+        if (tipoLista.equalsIgnoreCase("ArrayList")){
+            listaTareas =new ArrayList<String>();
+        }else{
+            listaTareas =new LinkedList<>();
+        }
+    }
+
+    public static void addTask(List<String> list, String task){
+        list.add(task);
+        System.out.println("Tarea añadida: "+task);
+    }
+
+    public static void removeTask(List<String> list,String task){
+        list.remove(task);
+        System.out.println("Tarea eliminada: "+task);
+    }
+    public static void completeTask(List<String> list,String task){
+        String completado="Tarea completada: ["+ task +"]";
+        list.set(list.indexOf(task), completado);
+        System.out.println(completado);
+    }
+
+    public static String[] getTasks(List<String> list){
+        String[] array=(String [])list.toArray();
+        return array;
+    }
 }
